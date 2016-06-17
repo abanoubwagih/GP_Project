30d830bdab95dca035a282923e52fc7be11fd739
AbanoubWagih <abanoubwagih@gmail.com>
2016-06-17 10:10:43 +0200

    welcome and log in

--- app/src/main/java/com/gmail/abanoubwagih/gp_project/MainActivity.java
+++ app/src/main/java/com/gmail/abanoubwagih/gp_project/MainActivity.java
@@ -2,17 +2,45 @@ package com.gmail.abanoubwagih.gp_project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 
 import com.gmail.abanoubwagih.gp_project.Log_in_auth.EmailPasswordActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CountDown countDown;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, EmailPasswordActivity.class));
 
+        countDown = new CountDown(5000, 1000);
+        countDown.start();
+
+    }
+
+    public class CountDown extends CountDownTimer {
+
+        /**
+         * @param millisInFuture    The number of millis in the future from the call
+         *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
+         *                          is called.
+         * @param countDownInterval The interval along the way to receive
+         *                          {@link #onTick(long)} callbacks.
+         */
+        public CountDown(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            startActivity(new Intent(MainActivity.this, EmailPasswordActivity.class));
+        }
     }
 }
