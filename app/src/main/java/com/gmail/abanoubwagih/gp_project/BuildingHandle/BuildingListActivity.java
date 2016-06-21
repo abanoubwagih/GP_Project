3388d013ac9a5206c643b2bbc14163d9779e7e38
AbanoubWagih <abanoubwagih@gmail.com>
2016-06-21 04:20:08 +0200

    sms handle + welcome and log + list v2

--- app/src/main/java/com/gmail/abanoubwagih/gp_project/BuildingHandle/BuildingListActivity.java
+++ app/src/main/java/com/gmail/abanoubwagih/gp_project/BuildingHandle/BuildingListActivity.java
@@ -1,5 +1,6 @@
 package com.gmail.abanoubwagih.gp_project.BuildingHandle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,26 +15,26 @@ public class BuildingListActivity extends AppCompatActivity {
 
 
     public static final String BUILDING_ID = "BUILDING_ID";
-    private List<Building> products = DataProvidingFromFirebase.buildingList;
+    private List<Building> buildings = DataProvidingFromFirebase.buildingList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building_list);
 
         BuildingListAdapter adapter = new BuildingListAdapter(
-                this, R.layout.list_item, products);
+                this, R.layout.list_item, buildings);
         ListView lv = (ListView) findViewById(R.id.listView);
         lv.setAdapter(adapter);
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Intent intent = new Intent(BuildingListActivity.this, DetailActivity.class);
-//
-//                Building building = products.get(position);
-//                intent.putExtra(BUILDING_ID, building.getBuildingId());
-//
-//                startActivity(intent);
+                Intent intent = new Intent(BuildingListActivity.this, BuildingDetailsActivty.class);
+
+                Building building = buildings.get(position);
+                intent.putExtra(BUILDING_ID, building.getBuildingId());
+
+                startActivity(intent);
             }
         });
     }
