4437f4d1c7245db253597d8a0f253a4cc95b6b4b
AbanoubWagih <abanoubwagih@gmail.com>
2016-06-26 04:40:06 +0200

    till List and basic

--- app/src/main/java/com/gmail/abanoubwagih/gp_project/BuildingHandle/BuildingDetailsActivty.java
+++ app/src/main/java/com/gmail/abanoubwagih/gp_project/BuildingHandle/BuildingDetailsActivty.java
@@ -15,10 +15,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.gmail.abanoubwagih.gp_project.Log_in_auth_and_Synchronous_Data.SynchronizeData;
+import com.gmail.abanoubwagih.gp_project.MapAndRouteHandler.MapsActivity;
 import com.gmail.abanoubwagih.gp_project.R;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.HashMap;
 
 import static com.gmail.abanoubwagih.gp_project.BuildingHandle.BuildingListActivity.BUILDING_ID;
 
@@ -72,12 +74,25 @@ public class BuildingDetailsActivty extends AppCompatActivity {
 
             }
 
-
+            final HashMap<String, Double> myGPS = building.getBuildingGPS();
 
             ImageView iv = (ImageView) findViewById(R.id.buildingImage);
             InputStream stream = new ByteArrayInputStream(Base64.decode(building.getPhoto().getBytes(), Base64.DEFAULT));
             Bitmap bitmap = BitmapFactory.decodeStream(stream);
             iv.setImageBitmap(bitmap);
+
+            iv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable(getString(R.string.BuildingGPS), myGPS);
+                    Intent intent = new Intent(BuildingDetailsActivty.this, MapsActivity.class);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+
+
+                }
+            });
 
         } catch (Exception e) {
             Log.e("not work detail", e.getMessage());
---
+++ app/src/main/java/com/gmail/abanoubwagih/gp_project/MapAndRouteHandler/MapsActivity.java
@@ -0,0 +1,65 @@
+package com.gmail.abanoubwagih.gp_project.MapAndRouteHandler;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.gmail.abanoubwagih.gp_project.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private HashMap<String, Double> GPS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        GPS = new HashMap<>();
+        Bundle bundle = getIntent().getExtras();
+        GPS.putAll((Map<? extends String, ? extends Double>) bundle.getSerializable(getString(R.string.BuildingGPS)));
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        GPS = new HashMap<>();
+        Bundle bundle = getIntent().getExtras();
+        GPS.putAll((Map<? extends String, ? extends Double>) bundle.getSerializable(getString(R.string.BuildingGPS)));
+        double lat = -34;
+        double log = 151;
+        if (!GPS.isEmpty() && GPS != null) {
+            lat = GPS.get(getString(R.string.GPSLat));
+            log = GPS.get(getString(R.string.GPSLog));
+        }
+
+        LatLng sydney = new LatLng(lat, log);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
